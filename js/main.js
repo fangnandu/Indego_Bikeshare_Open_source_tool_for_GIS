be549ba8a61ac574dbe9fa1892d36d3dc2750d72
Fangnan <fangnan@upenn.edu>
2018-05-07 16:32:55 -0400

    The final project
    The final project

---
+++ js/main.js
@@ -0,0 +1,420 @@
+
+var dataset = "https://raw.githubusercontent.com/kefanl/bikeshare_groupfinal/master/themeMapData/stationData.geojson";
+var phillyBoundary = "https://gist.githubusercontent.com/anonymous/eb5a6386acba87e14f8f4262cb4d0488/raw/7d073e2bab2e0a2cb55e1b19e0b9439bc833c886/boundary.json";
+var parsedData;
+$.ajax(dataset).done(function(data){
+  parsedData = JSON.parse(data);
+});
+
+$(document).ready(function() {
+  // Philly boundary
+  $.ajax(phillyBoundary).done(function(data) {
+    boundary = JSON.parse(data);
+    defaultBoundary();
+    });
+  });
+
+  var map = L.map('map', {
+    center: [39.96,-75.17],
+    zoom: 14
+  });
+
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+// the default boundary
+
+var defaultBoundary = function(event) {
+  var boundaryStyle = {
+    color:"white",
+    opacity:0.4,
+    weight:1,
+    fillOpacity:0,
+    dashArray: '5',
+  };
+  featureGroup = L.geoJson(boundary, {
+    style: boundaryStyle,
+  }).addTo(map);
+};
+
+
+$(document).ready(function() {
+  // Philly boundary
+  $.ajax(phillyBoundary).done(function(data) {
+    boundary = JSON.parse(data);
+    defaultBoundary();
+    // First plot philly boundary then plot markers - crucial for layer order
+    $.ajax(dataset).done(function(data) {
+      parsedData = JSON.parse(data);
+      defaultPage();
+    });
+  });
+});
+
+
+$('#route').click(function(e){
+  reset()
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+
+  defaultPage()
+
+});
+
+$('#info').click(function(e){
+  reset()
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+
+  defaultPage()
+
+});
+//default maps:
+var defaultPage = function(event) {
+
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by < a href=" ">Stamen Design</ a>, < a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</ a> &mdash; Map data &copy; < a href="http://www.openstreetmap.org/copyright">OpenStreetMap</ a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+  featureGroup = L.geoJson(parsedData, {
+    style: {color: "#50EBEC",
+            radius: 4,
+            fillColor: "#50EBEC",
+            weight: 4,
+            opacity: 0.4,
+            fillOpacity: 0.8},
+    onEachFeature:onEachFeature,
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    }
+  }).addTo(map);
+  map.setView([39.96,-75.17],14);
+};
+
+
+var removeMarker = function(){
+  if (typeof featureGroup !== 'undefined') {
+    map.removeLayer(featureGroup);
+  }
+};
+
+
+$('#Animation').click(function(e){
+  removeMarker();
+});
+
+var onEachFeature = function(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: clickEachFeature,
+    });
+};
+
+
+
+
+// Default interacive feature
+var highlightFeature = function(feature) {
+    var layer = feature.target;
+    layer.setStyle({radius: 12,
+                    color: "white",
+                    opacity: 0.5,
+                    fillColor: "white",
+                    fillOpacity: 0.8,
+    });
+    bindtext = "<dt>" + "Store ID: " + layer.feature.properties.cartodb_id + "</dt>" +
+               "<dt>" + "Address: " + layer.feature.properties.end_count + "</dt>" ;
+};
+
+var resetHighlight = function(feature) {
+    featureGroup.resetStyle(feature.target);
+};
+
+var clickEachFeature = function(feature) {
+
+    var x = document.getElementById("route-page");
+    if(x.style.display === "none"){
+    // hide all other pages
+    $('.intsidebar').fadeIn();
+    $('#stationInfo').fadeIn();
+    $('#maps-page').hide();
+    $("#filter-page").hide()
+    var layer = feature.target;
+    fillInfo(layer);
+  }else{
+    if(x.style.display !== "none"){
+      // hide all other pages
+      $('.intsidebar').fadeIn();
+      $('#stationInfo').hide();
+      $('#maps-page').hide();
+      $("#filter-page").hide()
+      var layer = feature.target;
+      fillInfo(layer);
+      /* =====================
+      Route
+      ===================== */
+      var state = {
+        position: {
+          marker: null,
+          updated: null
+        }
+      };
+
+      // Personized drop pin icon to mark current location
+      var myIcon = L.icon({
+        iconUrl: 'my-icon.png',
+        iconSize: [35, 30],
+        popupAnchor: [0, -20]
+      });
+
+      var myIcon2 = L.icon({
+        iconUrl: 'my-icon-default.png',
+        iconSize: [35, 30],
+        popupAnchor: [0, -20]
+      });
+
+      var updatePosition = function(lat, lng, updated) {
+        if (state.position.marker) { map.removeLayer(state.position.marker); }
+        state.position.marker = L.marker([lat, lng],{icon: myIcon});
+        state.position.updated = updated;
+        locationMarker = state.position.marker;
+        locationMarker.addTo(map).bindPopup('Your current location');
+      };
+
+      // Set the original location
+      var origin = {"lat":0, "lng":0};
+
+      // Only the first time user clicks the route page, we will ask if user allows for geolocating their position
+      $('#route-page').one("click", function () {
+        /* This 'if' check allows us to safely ask for the user's current position */
+        if ("geolocation" in navigator) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            updatePosition(position.coords.latitude, position.coords.longitude, position.timestamp);
+            // Set current position as the origin
+            origin.lat = position.coords.latitude;
+            origin.lng = position.coords.longitude;
+            console.log(origin);
+          });
+        } else {
+          alert("Allow us to know your current position to try the 'Route' page!");
+        }
+      });
+
+          var dest_lat = $('#lat').val();
+          var dest_lng = $('#lng').val();
+
+
+        // Check if the store ID is entered correctly
+          /* If fail to get correct current location, se default start point*/
+          if (origin.lng === 0 || origin.lat === 0) {
+            origin.lat = 39.952562;
+            origin.lng = -75.192620;
+            alerttext = "<dt>" + "Defalut location marker" + "</dt>" +
+                        "<dt>" + "(Shows if unable to get your location)" + "</dt>";
+            L.marker([origin.lat, origin.lng],{icon: myIcon2}).addTo(map).bindPopup(alerttext);
+          }
+          /* Otherwise, use user's current location as the origin */
+          var myToken = "pk.eyJ1Ijoibm9yYXlpbiIsImEiOiJjamZoYnVhajYzcWRjMnFvZnhkc2lkaDFnIn0.uwQxjsuwtL0epbau5U0M7Q";
+          var route = "https://api.mapbox.com/directions/v5/mapbox/driving/" + origin.lng + "," + origin.lat + ";" + dest_lng + "," + dest_lat + "?access_token=" + myToken;
+          $.ajax(route).done(function(data){
+            var eachRoute = decode(data.routes[0].geometry);
+            var latlngs = _.map(eachRoute, function(each) {return [each[1]*10, each[0]*10];});
+            // Map the route using turf.js
+            var line = turf.lineString(latlngs);
+            var routeStyle = {
+              "color": "pink",
+              "weight": 3,
+              "opacity": 0.7
+            };
+            generatedRoute = L.geoJson(line, {style: routeStyle}).addTo(map);
+          });
+
+          var removeLine = function(){
+            if (typeof generatedRoute !== 'undefined') {
+              map.removeLayer(generatedRoute);
+            }
+          };
+
+
+          $("#clear").click(function(e) {
+            $('#usr').val('');
+            removeLine();
+
+
+          });
+
+
+    }else{}
+
+
+
+  }
+
+
+};
+
+var fillInfo = function(layer) {
+  $('#station_id').val("Station ID: " + layer.feature.properties.stationid);
+  $('#station_name').val("Station Name: " + layer.feature.properties.station_name);
+  $('#total_num_rides').val("Total Num Rides: " + layer.feature.properties.totalcount);
+  $('#startcount').val("Departure Num Rides: " + layer.feature.properties.start_count);
+  $('#end_count').val("Arrival Num Rides: " + layer.feature.properties.end_count);
+  $('#onewaycount').val("Oneway Num Rides: " + layer.feature.properties.oneway_count);
+  $('#roundwaycount').val("Rounndway Num Rides: " + layer.feature.properties.roundway_count);
+  $('#inoutratio').val("in/out ratio: " + layer.feature.properties.inoutratio);
+  $('#onewayroundwayratio').val("oneway ratio: " + layer.feature.properties.oneway_ratio);
+  $('#lat').val(layer.feature.properties.lat);
+  $('#lng').val(layer.feature.properties.lng);
+
+
+};
+
+
+
+
+
+
+
+
+//animation part
+// Include this CSS so that torque knows what to do
+var CARTOCSS = [
+  'Map {',
+    '-torque-frame-count: 256;',
+    '-torque-animation-duration: 30;',
+    '-torque-time-attribute: "start_time";',
+    '-torque-aggregation-function: "count(1)";',
+    '-torque-resolution: 4;',
+    '-torque-data-aggregation: linear;',
+  '}',
+  '#layer {',
+    'marker-width: 7;',
+    'marker-fill: #7fb7d7;',
+    'marker-fill-opacity: 0.9;',
+    'marker-line-width: 1;',
+    'marker-line-color: #FFFFFF;',
+    'marker-line-opacity: 1;',
+    'comp-op: lighter;',
+  '}',
+  '#layer[frame-offset=1] {',
+    'marker-width: 9;',
+    'marker-fill-opacity: 0.45;',
+  '}',
+  '#layer[frame-offset=2] {',
+    'marker-width: 11;',
+    'marker-fill-opacity: 0.225;',
+  '}'
+].join('\n');
+
+// Create the actual layer to be used
+var torqueLayer = new L.TorqueLayer({
+  user: 'fangnandu',
+  cartocss: CARTOCSS,
+});
+
+$('#Animation').click(function(e){
+  featureGroup = torqueLayer.addTo(map);
+  torqueLayer.setSQL("SELECT * FROM dataanimation_1 ");
+  torqueLayer.play()
+});
+
+// On timechange, update the HTML which hovers over the upper right of the map
+torqueLayer.on('change:time', function(d) {
+  var time = $('<h3>').text('Time - ' + moment(d.time).format('HH:mm'));
+  $('div#time-window div').empty();
+  $('#time-window div').append(time);
+});
+$('#time-window')
+  .append($('<h1>').text('Date - 2017-12-1' ))
+  .append($('<div>'));
+
+/* =====================
+Basic Change of Slides
+===================== */
+function changeSildes() {
+  var x = document.getElementById("slide1");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+  }
+  var y = document.getElementById("slide2");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+  }
+}
+function backtoHome() {
+  var x = document.getElementById("slide2");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+  }
+  var y = document.getElementById("slide1");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+  }
+}
+function reShowIntro() {
+  analytical1.style.display = "block";
+}
+function seeVideo() {
+  var x = document.getElementById("analytical1");
+  x.style.display = "none";
+}
+//navigation
+$('.intsidebar').hide();
+$('#maps-page').hide();
+$('#stationInfo').fadeIn();
+$("#time-window").hide();
+$("#route-page").hide();
+
+
+$('#maps').click(function(e){
+  $('.intsidebar').fadeIn();
+  $('#maps-page').fadeIn();
+  $('#stationInfo').hide();
+  $("#route-page").hide();
+  $("#time-window").hide();
+});
+
+$('#Animation').click(function(e){
+  $('.intsidebar').hide();
+  $('#maps-page').hide();
+  $('#stationInfo').hide();
+  $("#route-page").hide();
+  $("#time-window").fadeIn();
+});
+
+$('#info').click(function(e){
+  $('.intsidebar').fadeIn();
+  $('#stationInfo').fadeIn();
+  $('#maps-page').hide();
+  $("#route-page").hide();
+  $("#time-window").hide();
+});
+
+
+$('#route').click(function(e){
+  $('.intsidebar').fadeIn();
+  $('#stationInfo').hide();
+  $('#maps-page').hide();
+  $("#route-page").fadeIn();
+  $("#time-window").hide();
+});
+
+/* =====================
+Change Slides
+===================== */
---
+++ js/themeMap.js
@@ -0,0 +1,385 @@
+
+
+// Getting data from Github(exported from CartoDB)
+
+
+
+var dataset = "https://raw.githubusercontent.com/kefanl/bikeshare_groupfinal/master/themeMapData/stationData.geojson";
+var featureGroup;
+
+
+titleLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+           maxZoom: 18,
+           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
+      }).addTo(map);
+
+
+      var nMap=function(){
+                titleLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+                     maxZoom: 18,
+                     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
+                }).addTo(mymap).bringToFront();
+            };
+var removeMarker = function(){
+  if (typeof featureGroup !== 'undefined') {
+    map.removeLayer(featureGroup);
+  }
+};
+var reset = function() {
+  removeMarker();
+  defaultPage();
+};
+
+var map1 = function(event) {
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+
+  featureGroup = L.geoJson(parsedData, {
+    style: defaultStyle1,
+    onEachFeature: defaultFeature1,
+    pointToLayer: function(feature, lnglat) {
+      return L.circleMarker(lnglat).bindPopup(feature.properties.station_name);
+    }
+  })
+  map.addLayer(featureGroup);
+
+};
+
+
+
+$('#mapsChoice1').click(function(e){
+  reset()
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+  $("#legend1").fadeIn();
+  $("#legend2").hide();
+  $("#legend3").hide();
+  $("#legend4").hide();
+  map1();
+
+});
+
+
+
+  var defaultColor1 = function(d) {
+      return d > 6400  ? '#ffffcc' :
+             d > 5000  ? '#a1dab4' :
+             d > 4000  ? '#68bfa1' :
+             d > 3000  ? '#2a9c9c' :
+             d > 2000  ? '#02778e' :
+                        '#045275';
+  };
+  var defaultRadius1 = function(d) {
+      return d > 6400  ? 24 :
+             d > 5000  ? 20 :
+             d > 4000  ? 16 :
+             d > 3000  ? 12 :
+             d > 2000  ? 8 :
+                        6;
+  };
+  var defaultStyle1 = function(feature) {
+      return {color: "white",
+              weight: 0,
+              radius: defaultRadius1(feature.properties.totalcount),
+              fillColor: defaultColor1(feature.properties.totalcount),
+              opacity: 0.5,
+              fillOpacity: 0.8,
+      };
+  };
+
+  //set popup for default page
+  var showName1 = function(feature) {
+      var layer1 = feature.target;
+      layer1.setStyle({radius: defaultRadius1(feature.properties.totalcount) + 10,
+                      color: "white",
+                      opacity: 0.5,
+                      fillColor: "white",
+                      fillOpacity: 0.4,
+      });
+      bindtext = "<dt>" + "Staiton Name: " + feature.properties.station_name + "</dt>";
+      layer.bindTooltip(bindtext,{opacity: 0.7, offset:[-10,0], direction:'left'}).openTooltip();
+  };
+  var hideName1 = function(feature) {
+      featureGroup.resetStyle(feature.target);
+  };
+
+  //set basic interaction for each station on default page
+  var defaultFeature1 = function(feature, layer) {
+      layer.on({
+          mouseover: showName1,
+          mouseout: hideName1,
+      });
+  };
+//console basic map showing the popylarity of each station based on its totalcount of trips
+
+
+
+
+
+var map2 = function(event) {
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+
+  var featureGroup = L.geoJson(parsedData, {
+    style: defaultStyle2,
+    onEachFeature: defaultFeature2,
+    pointToLayer: function(feature, lnglat) {
+      return L.circleMarker(lnglat).bindPopup(feature.properties.start_count);
+    }
+  })
+  map.addLayer(featureGroup);
+
+};
+
+$('#mapsChoice2').click(function(e){
+
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+  $("#legend1").hide();
+  $("#legend2").fadeIn();
+  $("#legend3").hide();
+  $("#legend4").hide();
+  map2();
+});
+//departurelayer
+
+
+
+var defaultRadius2 = function(d) {
+    return d > 6400  ? 24 :
+           d > 5000  ? 20 :
+           d > 4000  ? 16 :
+           d > 3000  ? 12 :
+           d > 2000  ? 8 :
+                      6;
+};
+var defaultStyle2 = function(feature) {
+    return {color: "orange",
+            weight: 0,
+            radius: defaultRadius2(feature.properties.start_count),
+            fillColor: "orange",
+            opacity: 0.5,
+            fillOpacity: 0.8,
+    };
+};
+
+//set popup for default page
+var showName2 = function(feature) {
+    var layer = feature.target;
+    layer.setStyle({radius: 24,
+                    color: "orange",
+                    opacity: 0.5,
+                    fillColor: "orange",
+                    fillOpacity: 0.4,
+    });
+    bindtext = "<dt>" + "Departuer Trips: " + feature.properties.start_count + "</dt>";
+    layer.bindTooltip(bindtext,{opacity: 0.7, offset:[-10,0], direction:'left'}).openTooltip();
+};
+var hideName2 = function(feature) {
+    featureGroup.resetStyle(feature.target);
+};
+
+//set basic interaction for each station on default page
+var defaultFeature2 = function(feature, layer) {
+    layer.on({
+        mouseover: showName2,
+        mouseout: hideName2,
+    });
+};
+
+
+var map3 = function(event) {
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+
+  var featureGroup = L.geoJson(parsedData, {
+    style: defaultStyle3,
+    onEachFeature: defaultFeature3,
+    pointToLayer: function(feature, lnglat) {
+      return L.circleMarker(lnglat).bindPopup(feature.properties.end_count);
+    }
+  })
+  map.addLayer(featureGroup);
+
+};
+
+$('#mapsChoice3').click(function(e){
+
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+  $("#legend1").hide();
+  $("#legend2").hide();
+  $("#legend3").fadeIn();
+  $("#legend4").hide();
+  map3();
+
+});
+
+
+var defaultRadius3 = function(d) {
+    return d > 5000  ? 24 :
+           d > 4000  ? 20 :
+           d > 3000  ? 16 :
+           d > 2000  ? 12 :
+           d > 1000  ? 8 :
+                      6;
+};
+var defaultStyle3 = function(feature) {
+    return {color: "lightblue",
+            weight: 0,
+            radius: defaultRadius3(feature.properties.end_count),
+            fillColor: "lightblue",
+            opacity: 0.5,
+            fillOpacity: 0.8,
+    };
+};
+
+//set popup for default page
+var showName3 = function(feature) {
+    var layer = feature.target;
+    layer.setStyle({radius: 24,
+                    color: "lightblue",
+                    opacity: 0.5,
+                    fillColor: "lightblue",
+                    fillOpacity: 0.4,
+    });
+    bindtext = "<dt>" + "Arrival Count: " + layer.feature.properties.end_count + "</dt>";
+    layer.bindTooltip(bindtext,{opacity: 0.7, offset:[-10,0], direction:'left'}).openTooltip();
+};
+var hideName3 = function(feature) {
+    featureGroup.resetStyle(feature.target);
+};
+
+//set basic interaction for each station on default page
+var defaultFeature3 = function(feature, layer) {
+    layer.on({
+        mouseover: showName3,
+        mouseout: hideName3,
+    });
+};
+
+
+var map4 = function(event) {
+  map.setView([39.96,-75.17],14);
+
+  var featureGroup = L.geoJson(parsedData, {
+    style: defaultStyle4,
+    onEachFeature: defaultFeature4,
+    pointToLayer: function(feature, lnglat) {
+      return L.circleMarker(lnglat).bindPopup(feature.properties.oneway_ratio);
+    }
+  })
+  map.addLayer(featureGroup);
+
+};
+
+var map4 = function(event) {
+
+
+
+  var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+  }).addTo(map);
+
+  var featureGroup = L.geoJson(parsedData, {
+    style: defaultStyle4,
+    onEachFeature: defaultFeature4,
+    pointToLayer: function(feature, lnglat) {
+      return L.circleMarker(lnglat).bindPopup(feature.properties.oneway_ratio);
+    }
+  })
+  map.addLayer(featureGroup);
+
+};
+
+
+
+$('#mapsChoice4').click(function(e){
+
+  removeMarker()
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer)
+  });
+  $("#legend1").hide();
+  $("#legend2").hide();
+  $("#legend3").hide();
+  $("#legend4").fadeIn();
+  map4()
+});
+
+
+
+var defaultColor4 = function(d) {
+    return d > 0.95  ? '#ffffcc' :
+           d > 0.90  ? '#a1dab4' :
+           d > 0.85  ? '#68bfa1' :
+           d > 0.80  ? '#2a9c9c' :
+           d > 0.70  ? '#02778e' :
+                      '#045275';
+};
+var defaultRadius4 = function(d) {
+    return d > 0.95  ? 24 :
+           d > 0.90  ? 20 :
+           d > 0.85  ? 16 :
+           d > 0.80  ? 12 :
+           d > 0.70  ? 8 :
+                      6;
+};
+var defaultStyle4 = function(feature) {
+    return {color: "white",
+            weight: 0,
+            radius: defaultRadius4(feature.properties.oneway_ratio),
+            fillColor: defaultColor4(feature.properties.oneway_ratio),
+            opacity: 0.5,
+            fillOpacity: 0.8,
+    };
+};
+
+//set popup for default page
+var showName4 = function(feature) {
+    var layer = feature.target;
+    layer.setStyle({radius: 24,
+                    color: "white",
+                    opacity: 0.5,
+                    fillColor: "white",
+                    fillOpacity: 0.4,
+    });
+    bindtext = "<dt>" + "Oneway Trip Ratio: " + layer.feature.properties.station_name + "</dt>";
+    layer.bindTooltip(bindtext,{opacity: 0.7, offset:[-10,0], direction:'left'}).openTooltip();
+};
+var hideName4 = function(feature) {
+    featureGroup.resetStyle(feature.target);
+};
+
+//set basic interaction for each station on default page
+var defaultFeature4 = function(feature, layer) {
+    layer.on({
+        mouseover: showName4,
+        mouseout: hideName4,
+    });
+};
